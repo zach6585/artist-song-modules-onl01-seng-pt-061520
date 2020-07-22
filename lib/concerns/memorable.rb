d34476562158620766e70019925cfdf54b3f1c59
zachary mines <zmines@wesleyan.edu>
2020-07-22 22:27:33 +0000

    Done.

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,11 @@
 module Memorable 
   module ClassMethods 
     def reset_all
-      all.clear
+      self.all.clear
     end
 
     def count
-      all.count
+      self.all.count
     end
   end 
   module InstanceMethods
